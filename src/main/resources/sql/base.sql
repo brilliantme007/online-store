21e50ecbfa6b297355a98c145a2439b246f2e78c
brilliantme007 <709094392@qq.com>
2019-11-05 17:35:51 +0800

    111

--- src/main/resources/sql/base.sql
+++ src/main/resources/sql/base.sql
@@ -1,4 +1,3 @@
------test
 /*
  Navicat Premium Data Transfer
 
@@ -12,24 +11,89 @@
  Target Server Version : 50720
  File Encoding         : 65001
 
- Date: 05/11/2019 17:01:12
+ Date: 05/11/2019 17:35:03
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for t_test
+-- Table structure for t_group
 -- ----------------------------
-DROP TABLE IF EXISTS `t_test`;
-CREATE TABLE `t_test`  (
+DROP TABLE IF EXISTS `t_group`;
+CREATE TABLE `t_group`  (
   `fid` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `fgroupname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`fid`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of t_test
+-- Records of t_group
 -- ----------------------------
-INSERT INTO `t_test` VALUES ('111');
+INSERT INTO `t_group` VALUES ('111', '组1');
+
+-- ----------------------------
+-- Table structure for t_group_permission
+-- ----------------------------
+DROP TABLE IF EXISTS `t_group_permission`;
+CREATE TABLE `t_group_permission`  (
+  `fid` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `r_group_id` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `r_permission_id` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`fid`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_group_permission
+-- ----------------------------
+INSERT INTO `t_group_permission` VALUES ('111', '111', '111');
+
+-- ----------------------------
+-- Table structure for t_group_user
+-- ----------------------------
+DROP TABLE IF EXISTS `t_group_user`;
+CREATE TABLE `t_group_user`  (
+  `fid` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `r_group_id` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `r_user_id` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`fid`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_group_user
+-- ----------------------------
+INSERT INTO `t_group_user` VALUES ('111', '111', '111');
+
+-- ----------------------------
+-- Table structure for t_permission
+-- ----------------------------
+DROP TABLE IF EXISTS `t_permission`;
+CREATE TABLE `t_permission`  (
+  `fid` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `fname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `furl` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`fid`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_permission
+-- ----------------------------
+INSERT INTO `t_permission` VALUES ('111', 'dd', '/test');
+
+-- ----------------------------
+-- Table structure for t_user
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user`;
+CREATE TABLE `t_user`  (
+  `fid` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `floginname` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `fpassword` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`fid`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_user
+-- ----------------------------
+INSERT INTO `t_user` VALUES ('111', 'user1', 'user1');
 
 SET FOREIGN_KEY_CHECKS = 1;
